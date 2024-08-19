4ee8885a932b128c0ed58e85d2b75ef8518fb84a
barisu <ishiim1227@gmail.com>
2024-08-19 21:53:01 +0900

    連続した名前の生成機能とPeopleオブジェクトの生成機能を分離する

--- test/src/helpers/person.py
+++ test/src/helpers/person.py
@@ -1,4 +1,3 @@
-import re
 from typing import Iterable, Optional
 
 from pydantic import BaseModel
@@ -9,68 +8,27 @@ class People(BaseModel):
     members: list[schemas.Person]
 
     @classmethod
-    def alphabetical_range(cls, begin: str, end: str) -> "People":
-        """
-        Create a People object from a range of alphabetical characters.
-
-        Args:
-            begin (str): The first alphabetical character.
-            end (str): The last alphabetical character.
-
-        Returns:
-            People: A People object with members from the range of alphabetical characters.
-
-        Example:
-            >>> People.alphabetical_range("A", "C")
-            People(members=[Person(name='A'), Person(name='B'), Person(name='C')])
-        """
-
-        VALID_INPUT_REGEX = r"^[A-Z]$"
-
-        errors = []
-
-        if not re.match(VALID_INPUT_REGEX, begin):
-            errors.append("begin must be a single uppercase alphabetical character")
-
-        if not re.match(VALID_INPUT_REGEX, end):
-            errors.append("end must be a single uppercase alphabetical character")
-
-        if begin > end:
-            errors.append("begin must be less than or equal to end")
-
-        if errors:
-            raise ValueError(f"{len(errors)} errors occurred:\n\n" + "\n".join(errors))
-
-        return cls(members=[schemas.Person(name=chr(i)) for i in range(ord(begin), ord(end) + 1)])
-
-    @classmethod
-    def generate(cls, count: int, names: Optional[Iterable[int]] = None, weights: Optional[Iterable[int]] = None) -> "People":
+    def generate(cls,names: Optional[Iterable[str]] = None, paymentWeights: Optional[Iterable[int]] = None) -> "People":
         """
         Create a People object with a specified number of members.
 
         Args:
-            count (int): The number of members to generate.
-
+            names (Optional[Iterable[int]]): A list of names for the members.
+            weights (Optional[Iterable[int]]): A list of weights for the members.
         Returns:
             People: A People object with the specified number of members.
 
         Example:
-            >>> People.generate(3)
-            People(members=[Person(name='A'), Person(name='B'), Person(name='C')])
+            >>> People.generate(names=["Alice", "Bob", "Charlie"], weights=[1, 2, 3])
+            People(members=[Person(name='Alice', paymentWeight=1), Person(name='Bob', paymentWeight=2), Person(name='Charlie', paymentWeight=3)])
         """
 
-        if count < 0:
-            raise ValueError("count must be greater than or equal to 0")
-        if names != None and len(names) != count:
-            raise ValueError("count must be equal to the length of names")
-        if weights != None and len(weights) != count:
-            raise ValueError("count must be equal to the length of weights")
+        if names == None and paymentWeights == None:
+            raise ValueError("names and weights cannot both be None")
 
-        if names != None and weights != None:
-            return cls(members=[schemas.Person(name=name,paymentWeight=weight) for name, weight in zip(names, weights) ])
+        if names != None and paymentWeights != None:
+            return cls(members=[schemas.Person(name=name,paymentWeight=weights) for name, weights in zip(names, paymentWeights) ])
         elif names != None:
             return cls(members=[schemas.Person(name=name) for name in names])
-        elif weights != None:
-            return cls(members=[schemas.Person(name=chr(i + ord('A')),paymentWeight=weights[i]) for i in range(count)])
-        else:
-            return cls(members=[schemas.Person(name=chr(i + ord('A')) ) for i in range(count)])
+        elif paymentWeights != None:
+            return cls(members=[schemas.Person(name=chr(i + ord('A')),paymentWeight=paymentWeights[i]) for i in range(len(paymentWeights))])
